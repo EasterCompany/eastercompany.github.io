44f45ad0482e001dee2021247554e23398e9fbdf
Dexter Easter <dexter@easter.company>
2026-02-27 18:08:09 -0500

    12.72.20 -> 12.72.21
    Co-authored-by: Owen Easter <owen@easter.company>

--- engine/main.js
+++ engine/main.js
@@ -15,10 +15,10 @@ if (buildHash) {
   localStorage.setItem('dex_build_hash', buildHash);
 }
 
-import { Engine } from './core/Engine.js?h=1772233543';
-import { RenderSystem } from './systems/RenderSystem.js?h=1772233543';
-import { UISystem } from './systems/UISystem.js?h=1772233543';
-import { ChatSystem } from './systems/ChatSystem.js?h=1772233543';
+import { Engine } from './core/Engine.js?h=1772233689';
+import { RenderSystem } from './systems/RenderSystem.js?h=1772233689';
+import { UISystem } from './systems/UISystem.js?h=1772233689';
+import { ChatSystem } from './systems/ChatSystem.js?h=1772233689';
 
 // Initialize the Engine
 const engine = new Engine('hero-canvas');
